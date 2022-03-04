3ea8e54803dbc6f4f3051dfcc530ab3015537ac2
Srila Palanikumar <srila.palanikumar@gmail.com>
2022-03-03 18:59:57 -0600

    Create march3.js

---
+++ march3.js
@@ -0,0 +1,12 @@
+//define function
+function triangleArea(base, height) {
+    return base * height * 0.5;
+}
+
+var area = triangleArea(5, 10);
+var areaTwo = triangleArea(2, 4);
+var areaThree = triangleArea(4, 20);
+
+console.log(area);
+console.log(areaTwo);
+console.log(areaThree);
